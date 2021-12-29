9061a32760d858a4b33c774740cdb310b7e9d09d
liql <liqianlong219@gmail.com>
2021-12-29 09:49:02 +0800

    Money页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,8 @@
 <template>
-<div>
-  Money.vue
-  <hr>
+<div class="nav-wrapper">
+<div class="content">
+
+</div>
   <Nav />
 </div>
 </template>
@@ -16,5 +17,15 @@ export default class Money extends Vue {
 </script>
 
 <style lang="scss" scoped>
+.nav-wrapper{
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  height:100vh;
+  .content{
+    flex-grow: 1;
+    overflow:auto;
+  }
+}
 
 </style>
