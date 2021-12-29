efeec4945daec296483e005320569dbd74edc5a9
liql <liqianlong219@gmail.com>
2021-12-29 13:09:43 +0800

    添加了Nav样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,16 +1,16 @@
 <template>
-<div>
-  <router-link to="/money">
+<nav>
+  <router-link to="/money" class="item">
     <Icon name="money" />
     记账
   </router-link>
-  <router-link to="/labels">
+  <router-link to="/labels" class="item">
     <Icon name="label" />
     标签</router-link>
-  <router-link to="/statistics">
+  <router-link to="/statistics" class="item">
     <Icon name="statistics" />
     统计</router-link>
-</div>
+</nav>
 </template>
 
 <script lang="ts">
@@ -24,5 +24,23 @@ export default class Nav extends Vue {
 </script>
 
 <style lang="scss" scoped>
+ nav{
+   display: flex;
+   box-shadow: 0 0 3px rgba(0,0,0,0.5);
+   font-size: 12px;
+  flex-direction: row;
+    > .item{
+      padding: 2px 0;
+      width:33.3333%;
+      display:flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      .icon{
+        width:32px;
+        height:32px;
+      }
 
+   }
+ }
 </style>
