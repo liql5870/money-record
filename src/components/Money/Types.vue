d1568f0485c0f531b15b69ea94e331f083271238
liql <liqianlong219@gmail.com>
2021-12-30 15:57:29 +0800

    完成Types组件

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -2,49 +2,51 @@
   <div>
     <ul class="types">
       <li :class=" type === '-' && 'selected' "
-           @click = "selectType('-')" >支出</li>
+          @click="selectType('-')">支出
+      </li>
       <li :class=" type === '+' && 'selected'"
-      @click = "selectType('+')">收入</li>
+          @click="selectType('+')">收入
+      </li>
     </ul>
-  </div></template>
+  </div>
+</template>
 
-<script >
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
 
-export default {
-  name:'Types',
-  data() {
-    return {
-      type: '-'  // "-"表示支出，"+"表示收入
+@Component
+export default class Types extends Vue {
+  type = '-';
+
+  selectType(type: string) {
+    if (type !== '-' && type !== '+') {
+      throw new Error('type is unknow');
     }
-  },
-  methods:{
-    selectType(type){  // type只能是“-”和“+”中的一个
-      if(type !== "-" && type !== "+"){
-        throw new Error ('type is unknow')
-      }
-      this.type = type
-    }
+    this.type = type;
   }
-
 }
+
 </script>
 
 <style lang="scss" scoped>
 
-.types{
-  background:#c4c4c4;
+.types {
+  background: #c4c4c4;
   display: flex;
   text-align: center;
   font-size: 24px;
-  > li{
+
+  > li {
     width: 50%;
     height: 64px;
     display: flex;
     justify-content: center;
     align-items: center;
     position: relative;
-    &.selected::after{
-      content:'';
+
+    &.selected::after {
+      content: '';
       position: absolute;
       bottom: 0;
       left: 0;
