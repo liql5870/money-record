ed1d71832ab7573015fbb32dc93f0edd20060294
liql <liqianlong219@gmail.com>
2022-01-01 22:21:38 +0800

    新增EditLabel组件

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import Money from '@/views/Money.vue'
 import Labels from "@/views/Labels.vue";
 import Statistics from "@/views/Statistics.vue";
 import NotFound from "@/views/NotFound.vue";
+import EditLabel from "@/views/EditLabel.vue";
 
 
 Vue.use(VueRouter)
@@ -33,6 +34,10 @@ const routes: Array<RouteConfig> = [
     path:'*',
     name:'NotFound',
     component:NotFound,
+  },
+  {
+    path:'/labels/edit/:id',
+    component:EditLabel,
   }
 
 ]
