5bd8fe5c30f42792405b6539093166e9150d29f0
liql <liqianlong219@gmail.com>
2022-01-03 10:51:13 +0800

    重新封装recordListMOdel

--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -1,15 +1,17 @@
 const localStorageKeyName = 'recordList'
 
 const recordListModel = {
+    data:[] as RecordItem[],
     clone(data:RecordItem[] |RecordItem){
         return JSON.parse(JSON.stringify(data))
     },
     fetch() {
-        return JSON.parse(window.localStorage.getItem('recordList') || '[]') as RecordItem[]
+      this.data =   JSON.parse(window.localStorage.getItem('recordList') || '[]') as RecordItem[]
+        return this.data
 
     },
-    save(data:RecordItem[]) {
-        return window.localStorage.setItem(localStorageKeyName, JSON.stringify(data))
+    save() {
+        return window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data))
     },
 
 }
