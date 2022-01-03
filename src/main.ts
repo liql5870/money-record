fceabe85dded46419614ab80576c1979624332e2
liql <liqianlong219@gmail.com>
2022-01-03 15:24:08 +0800

    封装record的增操作

--- src/main.ts
+++ src/main.ts
@@ -7,16 +7,25 @@ import Nav from "@/components/Nav.vue";
 import Layout from "@/components/Layout.vue";
 import Icon from "@/components/Icon.vue";
 import tagListModel from "@/models/tagListModel";
+import recordListModel from "@/models/recordListModel";
 
 Vue.config.productionTip = false
 Vue.component("Nav",Nav)
 Vue.component("Layout",Layout)
 Vue.component("Icon",Icon)
 
+// record store
+window.recordList =recordListModel.fetch()
+window.createRecord = (record:RecordItem) =>{
+  recordListModel.create(record)
+}
+
+
+
+// tag的增删改查
 window.tagList = tagListModel.fetch()
 window.findTag = (id:string) =>{
   return window.tagList.filter(t =>t.id===id)[0]
-
 }
 window.createTag = (name:string) =>{
   if(name){
@@ -26,17 +35,13 @@ window.createTag = (name:string) =>{
     }else if(message === 'success'){
       window.alert('添加成功')
     }
-
   }
 };
 window.removeTag = (id:string)=>{
   return tagListModel.remove(id);
-
 }
-
 window.updateTag = (id:string,name:string) =>{
   return tagListModel.update(id,name) ;
-
 }
 
 
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -19,11 +19,9 @@ import NumberPad from "@/components/Money/NumberPad.vue";
 import Types from "@/components/Money/Types.vue";
 import FormItem from "@/components/Money/FormItem.vue";
 import Tags from "@/components/Money/Tags.vue";
-import {Component, Watch} from "vue-property-decorator";
-import  recordListModel  from "@/models/recordListModel";
+import {Component} from "vue-property-decorator";
 
 
-const recordList = recordListModel.fetch()
 @Component(
     {
       components: {FormItem, Tags, Types, NumberPad,},
@@ -32,7 +30,7 @@ const recordList = recordListModel.fetch()
 export default class Money extends Vue{
    tags=window.tagList;
   // eslint-disable-next-line no-undef
-   recordList:RecordItem[] = recordList;
+   recordList = window.recordList;
   // eslint-disable-next-line no-undef
    record:RecordItem = {
      tags:[],
@@ -49,13 +47,8 @@ export default class Money extends Vue{
     this.record.notes = value
    }
    saveRecord(){
-    recordListModel.create(this.record)
-
+    window.createRecord(this.record)
    }
-  @Watch('recordList')
-     onRecordListChange(){
-      recordListModel.save()
-     }
 
 }
 </script>
