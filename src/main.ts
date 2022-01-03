529706da7f7e5fabca4dada1e08de3b2b658c969
liql <liqianlong219@gmail.com>
2022-01-03 15:59:55 +0800

    把所有数据收集在store中

--- src/main.ts
+++ src/main.ts
@@ -6,49 +6,15 @@ import store from './store'
 import Nav from "@/components/Nav.vue";
 import Layout from "@/components/Layout.vue";
 import Icon from "@/components/Icon.vue";
-import tagListModel from "@/models/tagListModel";
-import recordListModel from "@/models/recordListModel";
 
 Vue.config.productionTip = false
-Vue.component("Nav",Nav)
-Vue.component("Layout",Layout)
-Vue.component("Icon",Icon)
-
-// record store
-window.recordList =recordListModel.fetch()
-window.createRecord = (record:RecordItem) =>{
-  recordListModel.create(record)
-}
-
-
-
-// tag的增删改查
-window.tagList = tagListModel.fetch()
-window.findTag = (id:string) =>{
-  return window.tagList.filter(t =>t.id===id)[0]
-}
-window.createTag = (name:string) =>{
-  if(name){
-    const message = tagListModel.create(name);
-    if(message === 'duplicated'){
-      window.alert('标签名重复啦')
-    }else if(message === 'success'){
-      window.alert('添加成功')
-    }
-  }
-};
-window.removeTag = (id:string)=>{
-  return tagListModel.remove(id);
-}
-window.updateTag = (id:string,name:string) =>{
-  return tagListModel.update(id,name) ;
-}
-
-
+Vue.component("Nav", Nav)
+Vue.component("Layout", Layout)
+Vue.component("Icon", Icon)
 
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
