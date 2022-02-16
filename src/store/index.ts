9670138d6b93eca79a5c77dfa3c4d5519daa9b5d
liql <liqianlong219@gmail.com>
2022-02-16 18:25:14 +0800

    修改无法删除bug

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,8 @@
-import Vue from "vue";
-import Vuex from "vuex";
-import clone from "@/lib/clone";
-import createId from "@/lib/createId";
-import router from "@/router";
+import Vue from 'vue';
+import Vuex from 'vuex';
+import clone from '@/lib/clone';
+import createId from '@/lib/createId';
+import router from '@/router';
 
 Vue.use(Vuex);
 
@@ -12,29 +12,29 @@ const store = new Vuex.Store({
     createRecordError: null,
     createTagError: null,
     tagList: [],
-    currentTag: undefined,
+    currentTag: undefined
   } as RootState,
   mutations: {
     setCurrentTag(state, id: string) {
-      state.currentTag = state.tagList.filter((t) => t.id === id)[0];
+      state.currentTag = state.tagList.filter(t => t.id === id)[0];
     },
-    updateTag(state, payload: { id: string; name: string }) {
-      const { id, name } = payload;
-      const idList = state.tagList.map((item) => item.id);
+    updateTag(state, payload: { id: string, name: string }) {
+      const {id, name} = payload;
+      const idList = state.tagList.map(item => item.id);
       if (idList.indexOf(id) >= 0) {
-        const names = state.tagList.map((item) => item.name);
+        const names = state.tagList.map(item => item.name);
         if (names.indexOf(name) >= 0) {
-          window.alert("标签名重复了");
+          window.alert('标签名重复了');
         } else {
-          const tag = state.tagList.filter((item) => item.id === id)[0];
+          const tag = state.tagList.filter(item => item.id === id)[0];
           tag.name = name;
-          store.commit("saveTags");
+          store.commit('saveTags');
         }
       }
     },
     removeTag(state, id: string) {
       let index = -1;
-      for (let i = 0; i < this.tagList.length; i++) {
+      for (let i = 0; i < state.tagList.length; i++) {
         if (state.tagList[i].id === id) {
           index = i;
           break;
@@ -42,61 +42,50 @@ const store = new Vuex.Store({
       }
       if (index >= 0) {
         state.tagList.splice(index, 1);
-        store.commit("saveTags");
+        store.commit('saveTags');
         router.back();
       } else {
-        window.alert("删除失败");
+        window.alert('删除失败');
       }
+
     },
     fetchRecords(state) {
-      state.recordList = JSON.parse(
-        window.localStorage.getItem("recordList") || "[]"
-      ) as RecordItem[];
+      state.recordList = JSON.parse(window.localStorage.getItem('recordList') || '[]') as RecordItem[];
     },
-    createRecord(state, record) {
-      const record2: RecordItem = clone(record);
+    createRecord(state, record: RecordItem) {
+      const record2 = clone(record);
       record2.createdAt = new Date().toISOString();
       state.recordList.push(record2);
-      store.commit("saveRecords");
-      // recordStore.saveRecords()
-      //   window.alert("已保存");
+      store.commit('saveRecords');
     },
     saveRecords(state) {
-      window.localStorage.setItem(
-        "recordList",
-        JSON.stringify(state.recordList)
-      );
+      window.localStorage.setItem('recordList',
+        JSON.stringify(state.recordList));
     },
     fetchTags(state) {
-      state.tagList = JSON.parse(
-        window.localStorage.getItem("tagList") || "[]"
-      );
+      state.tagList = JSON.parse(window.localStorage.getItem('tagList') || '[]');
       if (!state.tagList || state.tagList.length === 0) {
-        store.commit("createTag", "衣");
-        store.commit("createTag", "食");
-        store.commit("createTag", "住");
-        store.commit("createTag", "行");
+        store.commit('createTag', '衣');
+        store.commit('createTag', '食');
+        store.commit('createTag', '住');
+        store.commit('createTag', '行');
       }
     },
     createTag(state, name: string) {
       state.createTagError = null;
-      const names = state.tagList.map((item) => item.name);
+      const names = state.tagList.map(item => item.name);
       if (names.indexOf(name) >= 0) {
-        state.createTagError = new Error("tag name duplicated");
+        state.createTagError = new Error('tag name duplicated');
         return;
       }
       const id = createId().toString();
-      state.tagList.push({ id, name: name });
-      store.commit("saveTags");
-      return "success";
+      state.tagList.push({id, name: name});
+      store.commit('saveTags');
     },
     saveTags(state) {
-      window.localStorage.setItem("tagList", JSON.stringify(state.tagList));
+      window.localStorage.setItem('tagList', JSON.stringify(state.tagList));
     },
-    findTag(state, id: string) {
-      return state.tagList.filter((t) => t.id === id)[0];
-    },
-  },
+  }
 });
 
 export default store;
